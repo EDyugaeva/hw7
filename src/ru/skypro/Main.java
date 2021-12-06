77b725fe71b239d61f6d84edf30c93d3e65fc599
Ekaterina <ed2408@yandex.ru>
2021-12-06 19:31:21 +0300

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,15 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        task1();
+    }
+
+    public static void task1() {
+        
+
+
+    }
+}
