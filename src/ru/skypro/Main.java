0690f0a5debad8e4a38fdd2e98b328895ca689db
Ekaterina <ed2408@yandex.ru>
2021-12-11 11:58:05 +0300

    hw

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,23 +1,30 @@
 package ru.skypro;
 
-import java.util.Locale;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        task1();
-        task4();
+//        task1();
+//        task4();
+//        task5();
+//        task6();
+        task7();
     }
 
 
-        public static void task1() {
+    public static void task1() {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
-        String fullNameToReport = fullName.toUpperCase(Locale.ROOT);
+
+
+        String fullNameToReport = fullName.toUpperCase();
         System.out.println("ФИО сотрудника для отчета " + fullNameToReport);
+
+
         String fullNameToAdminDirection = fullName.replace(" ", "; ");
         System.out.println("Данные ФИО сотрудника для административного отдела - " + fullNameToAdminDirection);
 
@@ -25,11 +32,53 @@ public class Main {
 
     public static void task4() {
         String fullName = "Иванов Семён Семёнович";
-        if (fullName.contains("ё")) {
-            fullName = fullName.replace("ё", "е");
-        }
+//        if (fullName.contains("ё")) {
+        fullName = fullName.replace("ё", "e");
+//        }
         System.out.println("Данные ФИО сотрудника - " + fullName);
 
     }
+
+    public static void task5() {
+        String name = "Ivanov Ivan Ivanovich";
+        String[] words = name.split(" ");
+//        String middleName = words[2];
+//        String lastName = words[0];
+//        String firstName = words[1];
+
+        String lastName = name.substring(0, name.indexOf(" "));
+        String firstName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
+        String middleName = name.substring(name.lastIndexOf(" ") + 1);
+
+        System.out.println("Имя сотрудника - " + firstName);
+        System.out.println("Фамилия сотрудника - " + lastName);
+        System.out.println("Отчество сотрудника - " + middleName);
+
+    }
+
+    public static void task6() {
+        String fullName = "ivanov ivan ivanovich";
+        char[] rightName = fullName.toCharArray();
+        rightName[0]=Character.toUpperCase(rightName[0]);
+        for (int i = 0; i < rightName.length; i++) {
+            char s = rightName[i];
+            if (s == 32) {
+                rightName[i + 1] = Character.toUpperCase(rightName[i + 1]);
+            }
+            System.out.print(rightName[i]);
+        }
+//        String fullName1 = Arrays.toString(rightName);
+//        System.out.println("Верное написание ФИО сотрудника " + fullName1);
+
+    }
+    public static void task7() {
+        String first = "135";
+        String second = "246";
+        StringBuilder result = new StringBuilder();
+
+
+
+    }
+
 
 }
